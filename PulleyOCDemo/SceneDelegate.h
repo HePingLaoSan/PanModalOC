3dc590505b683d2a3b633b48be13c1fb0b57f4e0
zyj <kylinzhangyingjie@gmail.com>
2019-10-23 13:10:18 +0800

    Initial Commit

---
+++ PulleyOCDemo/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  PulleyOCDemo
+//
+//  Created by 张英杰 on 2019/10/23.
+//  Copyright © 2019 张英杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ PulleyOCDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PulleyOCDemo
+//
+//  Created by 张英杰 on 2019/10/23.
+//  Copyright © 2019 张英杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
